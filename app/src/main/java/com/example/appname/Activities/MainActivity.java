ce30d79a1f79830bfaf06a63db184bc37a71e615
MUSHFIQ SALEHIN <chowdhurymushfiqsalehin@gmail.com>
2019-07-07 05:33:59 +0600

    new url

--- app/src/main/java/com/example/appname/Activities/MainActivity.java
+++ app/src/main/java/com/example/appname/Activities/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         webView = findViewById(R.id.webView);
         progressBar = findViewById(R.id.progress_horizontal);
 
-        String url = "http://34.208.173.12";
+        String url = "http://34.208.173.12/idss/";
         /*webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(url);*/
         showTheWebsite(url);
